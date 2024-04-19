9c69ac84fdd13c0e06b9ec53a6402262b280af2c
Daniel <daniramirezva@gmail.com>
2024-04-19 12:28:16 -0500

    Perspectivas de hassam

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -25,6 +25,11 @@ int main()
     system("color e0");											//linea cambio de color a fondo amarillo
 	color(hConsole, 224);										//linea cambio de color a letra negra y fondo amarillo
     player player1,player2;										//declaracion de variables
+    bool ganador=false;
+    char tablero[7][7];
+    int i,j;
+    
+    
     
     player1.alfombras=10;										//establecer valores iniciales
     player2.alfombras=10;
@@ -34,7 +39,32 @@ int main()
 	cin>>player1.nombre;
 	cout<<"Digite el nombre del segundo jugador!"<<endl;
 	cin>>player2.nombre;
+	cout<<"SE LE OTORGA 10 ALFOMBRAS Y 10 MONEDAS A CADA JUGADOR!!"<<endl<<endl<<endl<<"HASSAM ESTA REPRESENTADO POR: "<<endl<<"Hassam hacia arriba: "<<char(193)<<endl<<"Hassam hacia la derecha: "<<char(195)<<endl<<"Hassam hacia abajo: "<<char(194)<<endl<<"Hassam hacia la izquierda: "<<char(180)<<endl<<endl<<endl<<"QUE EMPIECE EL JUEGO!!!"<<endl<<endl;
 	
+	
+	for(i=0;i<7;i++)
+	{
+		for(j=0;j<7;j++)
+		{
+			tablero[i][j]=char(0xDB);
+			if((i==3)&&(j==3))
+			{
+				tablero[i][j]=char(194);
+			}
+		}
+	}
+	while(ganador==false)
+	{
+		for(i=0;i<7;i++)
+		{
+			for(j=0;j<7;j++)
+			{
+				cout<<tablero[i][j]<<"   ";
+			}
+			cout<<endl<<endl;
+		}
+		ganador=true;
+	}
 
 	color(hConsole, 224);
 	return 0;
